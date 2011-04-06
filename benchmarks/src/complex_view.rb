f5e5b131735f805bbfad9e33956e56836ee499cf
Michel Martens <michel@soveran.com>
2011-04-05 23:53:33 -0300

    Add memoization to items in benchmarks.

--- benchmarks/src/complex_view.rb
+++ benchmarks/src/complex_view.rb
@@ -6,10 +6,13 @@ class ComplexView
   end
 
   def item
-    items = []
-    items << { :name => 'red', :current => true, :url => '#red' }
-    items << { :name => 'green', :current => false, :url => '#green' }
-    items << { :name => 'blue', :current => false, :url => '#blue' }
-    items
+    unless defined? @items
+      @items ||= []
+      @items << { :name => 'red', :current => true, :url => '#red' }
+      @items << { :name => 'green', :current => false, :url => '#green' }
+      @items << { :name => 'blue', :current => false, :url => '#blue' }
+    end
+
+    @items
   end
 end
