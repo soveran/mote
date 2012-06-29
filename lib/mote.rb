8c4be44c763a26835006c7ec88f37a55fcf1188a
Cyril David <cyx.ucron@gmail.com>
2012-06-30 03:50:50 +0800

    Release 1.0.0.rc1.
    This release removes the whole context fiasco in favor
    of just a global, no-assumption, no-context binding.

--- lib/mote.rb
+++ lib/mote.rb
@@ -18,7 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 class Mote
-  VERSION = "0.2.4"
+  VERSION = "1.0.0.rc1"
 
   PATTERN = /^\s*(%)(.*?)$|(<\?)\s+(.*?)\s+\?>|(\{\{)(.*?)\}\}/m
 
