aab3e9b6768ed232b96040861d43eb08b109823f
Michel Martens <michel@soveran.com>
2012-10-01 17:59:48 -0300

    Release.

--- lib/mote.rb
+++ lib/mote.rb
@@ -18,7 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 class Mote
-  VERSION = "1.1.0"
+  VERSION = "1.1.1"
 
   PATTERN = /^(\n+)|^\s*(%)\s*(.*?)\n|(<\?)\s+(.*?)\s+\?>|(\{\{)(.*?)\}\}/m
 
