94a97ffacab743fa35e00831f72527322d64661b
Michel Martens <michel@soveran.com>
2017-08-05 20:08:44 +0200

    Release

--- lib/mote.rb
+++ lib/mote.rb
@@ -18,7 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 class Mote
-  VERSION = "1.2.0"
+  VERSION = "1.3.0"
 
   # The regex has three alternative blocks that capture the embedded
   # Ruby code. The rest is left as is.
